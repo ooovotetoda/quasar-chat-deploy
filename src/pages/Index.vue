a5f0bcef11bcd5bc59c74267bffe2ec9fc47cca9
ooovotetoda <dima.rolick22@gmail.com>
2024-06-10 20:37:25 +0300

    fix: clear start template

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,40 +5,10 @@
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models'
-
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'PageIndex',
-  data () {
-    const todos: Todo[] = [
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]
-    const meta: Meta = {
-      totalCount: 1200
-    }
-    return { todos, meta }
-  }
+  name: 'PageIndex'
 })
 
 </script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,9 +8,6 @@ const routes: RouteConfig[] = [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '*',
     component: () => import('pages/Error404.vue')
