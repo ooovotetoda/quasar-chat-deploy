4ed5f9905c931232dbceb2dfbb266686e6eb5e05
Dmitry <47058524+ooovotetoda@users.noreply.github.com>
2024-06-24 14:41:38 +0300

    build: update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -119,7 +119,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v1.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: 'InjectManifest', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
         name: 'Chat',
@@ -128,7 +128,7 @@ module.exports = configure(function (/* ctx */) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#ffffff',
         icons: [
           {
             src: 'icons/icon-128x128.png',
